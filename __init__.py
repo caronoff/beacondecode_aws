5ee22c719bc7686a4472970facde61d063951286
Craig Aronoff <caronoff@gmail.com>
2017-10-23 15:13:40 -0400

    change to post submit

--- __init__.py
+++ __init__.py
@@ -31,7 +31,7 @@ def validatehex():
 
     return jsonify(echostatus=statuscheck, message=message)
 
-@app.route('/output', methods=['GET','POST'])
+@app.route('/<hexcode>', methods=['GET','POST'])
 def output():
     ## output will only process if hexcode has been validated.
 
