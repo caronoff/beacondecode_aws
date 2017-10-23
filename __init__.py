9cc666a03caa09607ee95011459fe70b10cfb07c
Craig Aronoff <caronoff@gmail.com>
2017-10-23 15:01:06 -0400

    change to post submit

--- __init__.py
+++ __init__.py
@@ -20,10 +20,10 @@ def validatehex():
         if len(hexaPattern)==len(ret_data):
 
             message='This is a valid hexidecimal message.'
-            if len(ret_data) in [15,22,30,23,63]:
+            if len(ret_data) in [15,30,23,63]:
                 statuscheck = 'valid'
             else:
-                message += '  However, length '+str(len(ret_data)) + ' is not a valid message.  Valid message should be 15,22,23,30 or 63.'
+                message += '  However, length '+str(len(ret_data)) + ' is not a valid message.  Valid message should be 15,23,30 or 63.'
         else:
             statuscheck='not valid'
             message='Invalid Hexidecimal code.  (A-F-0-9)'
@@ -37,21 +37,22 @@ def output():
     outputdata="dummy"
     if request.method== 'GET':
         hexcode= str(request.args.get('hexcode'))
-        username = str(request.args.get('username'))
-        if len(hexcode) == 63 or len(hexcode) == 51 or len(hexcode) == 75 or len(hexcode) == 23:
-            beacon = Gen2.SecondGen(hexcode)
-        else:
-            beacon = decodehex2.BeaconHex(hexcode)
-        print(hexcode)
-        beacon.processHex(hexcode)
-        ctry = beacon.country()
-        mid = str(ctry[0][1])
-        name = str(ctry[1][1])
-        print(name)
-        decoded = beacon.tablebin
-        return render_template('output.html',hexcode=hexcode,decoded=decoded)
+    elif request.method == 'POST':
+        hexcode = str(request.form('hexcode'))
+
+    if len(hexcode) == 63 or len(hexcode) == 51 or len(hexcode) == 75 or len(hexcode) == 23:
+        beacon = Gen2.SecondGen(hexcode)
     else:
-        return render_template('child.html', title='Home', user="")
+        beacon = decodehex2.BeaconHex(hexcode)
+
+    beacon.processHex(hexcode)
+    ctry = beacon.country()
+    mid = str(ctry[0][1])
+    name = str(ctry[1][1])
+
+    decoded = beacon.tablebin
+    return render_template('output.html',hexcode=hexcode,decoded=decoded)
+
 
 
 @app.route("/")
