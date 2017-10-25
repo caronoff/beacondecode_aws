5e4c0f1652552bb6fdfb298d62b59f8f6b1f5dc8
Craig Aronoff <caronoff@gmail.com>
2017-10-25 14:31:00 -0400

    change to post submit

--- static/js/gmap.js
+++ static/js/gmap.js
@@ -11,7 +11,7 @@ function initMap() {
             mapTypeId: google.maps.MapTypeId.ROADMAP
         }
         var map = new google.maps.Map(mapCanvas, mapOptions);
-        var markerImage = "{{ url_for('static', filename='images/marker.png') }}";
+        var markerImage = "http://maps.google.com/mapfiles/kml/shapes/firedept.png";
 
         var marker = new google.maps.Marker({
             position: location,
