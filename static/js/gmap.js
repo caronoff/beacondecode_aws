6648ade7330b360c497f5f8e7c5dd7e6848bcc84
Craig Aronoff <caronoff@gmail.com>
2017-10-25 12:01:52 -0400

    change to post submit

--- static/js/gmap.js
+++ static/js/gmap.js
@@ -1,8 +1,5 @@
-$(function () {
-    function initMap() {
-
+function initMap() {
         var location = new google.maps.LatLng({{ geocoord[0]}}, {{ geocoord[1]}});
-
         var mapCanvas = document.getElementById('map');
         var mapOptions = {
             center: location,
@@ -12,7 +9,6 @@ $(function () {
             mapTypeId: google.maps.MapTypeId.ROADMAP
         }
         var map = new google.maps.Map(mapCanvas, mapOptions);
-
         var markerImage = "{{ url_for('static', filename='images/marker.png') }}";
 
         var marker = new google.maps.Marker({
@@ -20,6 +16,10 @@ $(function () {
             map: map,
             icon: markerImage
         });
+
+        var styles = [{"featureType": "landscape", "stylers": [{"saturation": -100}, {"lightness": 65}, {"visibility": "on"}]}, {"featureType": "poi", "stylers": [{"saturation": -100}, {"lightness": 51}, {"visibility": "simplified"}]}, {"featureType": "road.highway", "stylers": [{"saturation": -100}, {"visibility": "simplified"}]}, {"featureType": "road.arterial", "stylers": [{"saturation": -100}, {"lightness": 30}, {"visibility": "on"}]}, {"featureType": "road.local", "stylers": [{"saturation": -100}, {"lightness": 40}, {"visibility": "on"}]}, {"featureType": "transit", "stylers": [{"saturation": -100}, {"visibility": "simplified"}]}, {"featureType": "administrative.province", "stylers": [{"visibility": "off"}]}, {"featureType": "water", "elementType": "labels", "stylers": [{"visibility": "on"}, {"lightness": -25}, {"saturation": -100}]}, {"featureType": "water", "elementType": "geometry", "stylers": [{"hue": "#ffff00"}, {"lightness": -25}, {"saturation": -97}]}];
+
+        map.set('styles', styles);
 
         var contentString = '<div class="info-window">' +
                 '<h3>Alert!</h3>' +
@@ -37,9 +37,6 @@ $(function () {
             infowindow.open(map, marker);
         });
 
-        var styles = [{"featureType": "landscape", "stylers": [{"saturation": -100}, {"lightness": 65}, {"visibility": "on"}]}, {"featureType": "poi", "stylers": [{"saturation": -100}, {"lightness": 51}, {"visibility": "simplified"}]}, {"featureType": "road.highway", "stylers": [{"saturation": -100}, {"visibility": "simplified"}]}, {"featureType": "road.arterial", "stylers": [{"saturation": -100}, {"lightness": 30}, {"visibility": "on"}]}, {"featureType": "road.local", "stylers": [{"saturation": -100}, {"lightness": 40}, {"visibility": "on"}]}, {"featureType": "transit", "stylers": [{"saturation": -100}, {"visibility": "simplified"}]}, {"featureType": "administrative.province", "stylers": [{"visibility": "off"}]}, {"featureType": "water", "elementType": "labels", "stylers": [{"visibility": "on"}, {"lightness": -25}, {"saturation": -100}]}, {"featureType": "water", "elementType": "geometry", "stylers": [{"hue": "#ffff00"}, {"lightness": -25}, {"saturation": -97}]}];
 
-        map.set('styles', styles);
-    }
-    google.maps.event.addDomListener(window, 'load', initMap);
-});
+       
+      }
