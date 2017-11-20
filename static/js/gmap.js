5cee60569fc726f7b1836619f3357469ccdb6d36
Craig Aronoff <caronoff@gmail.com>
2017-11-20 12:47:23 -0500

    working autocomplete trying something

--- static/js/gmap.js
+++ static/js/gmap.js
@@ -1,8 +1,6 @@
 function initMap() {
         var geocode = JSON.parse(document.getElementById("geocoord").dataset.geocode);
 
-
-
         var location = new google.maps.LatLng(geocode[0], geocode[1]);
         var mapCanvas = document.getElementById('map');
         var mapOptions = {
