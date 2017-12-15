db9a51a0fd0623487cfd3e9531c5243e70d0fdd5
Craig Aronoff <caronoff@gmail.com>
2017-12-15 09:58:45 -0500

    redirect for g008

--- __init__.py
+++ __init__.py
@@ -152,4 +152,9 @@ def decoded(hexcode):
 
 
 if __name__ == "__main__":
+    app.secret_key = 'super secret key'
+    app.config['SESSION_TYPE'] = 'filesystem'
+
+    sess.init_app(app)
+
     app.run(debug=True,host='0.0.0.0', port=5555)
