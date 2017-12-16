65f755e77da3b12d0a38b56edf0c241ebf4cb3a9
caronoff <caronoff@gmail.com>
2017-12-16 18:05:46 -0500

    redirects and detect if UIN when encode

--- longfirstgenmsg.py
+++ longfirstgenmsg.py
@@ -6,7 +6,7 @@ import time
 
 
 
-from decodefunctions import calcbch, bin2hex
+from decodefunctions import calcbch, bin2hex,dec2bin
 from random import randint
 
 def myround(x, base=5):
@@ -74,16 +74,11 @@ def coord2bin(latcoord, longcoord, resolution, ns=0, ew=0):
                 fine=lat[2] + lat[3] + lat[4] + long[2] + long[3] + long[4])
 
 
-def decode(hex_code):
+def decode(hex_code,latitude,southnorth,longitude,eastwest):
     c = decodehex2.BeaconHex()
     try:
         c.processHex(str(hex_code.strip()))
-        latitude = randint(0, 89999)
-        longitude = randint(0, 179999)
-        southnorth = randint(0, 1)
-        eastwest = randint(0, 1)
-        tsouthnorth = ('North', 'South')[southnorth]
-        teastwest = ('East', 'West')[eastwest]
+
 
         if c.protocolflag() == 'User':
             bch = calcbch(c.bin[:25] + '1' + c.bin[26:86], "1001101101100111100011", 25, 86, 107)
@@ -104,6 +99,41 @@ def decode(hex_code):
             bch2 = calcbch(binstr, '1010100111001', 107, 133, 145)
             binstr = binstr + bch2
 
+
+        elif c.protocolflag() == 'Location' and c.loctype()=='ELT-DT Location':
+            lat=round(float(latitude/1000)*2,0)
+
+
+            long=round(float(longitude/1000)*2,0)
+
+
+            binlat=dec2bin(lat,8)
+            binlong = dec2bin(long, 9)
+            binstr = c.bin[0:25] + '1' + c.bin[26:67] + str(southnorth) + binlat + str(eastwest) + binlong
+            bch1 = calcbch(binstr, "1001101101100111100011", 25, 86, 107)
+            binstr = binstr +bch1 + '11111111'
+            print(float(latitude/1000))
+            print(float(lat/2))
+            dif= float(latitude/1000) - float(lat/2)
+            if (dif) > 0:
+                sign= 1
+                print('add')
+            else:
+                print('subtract')
+                sign=-1
+            dif=abs(dif)
+            print(dif)
+            sec= int(float(dif*3600))
+            print('seconds '+ str(sec))
+            min= int(float(sec/60))
+            print('min' + str(min))
+            remaining_sec = sec - min*60
+            print(remaining_sec)
+            print('4sec ' + str(int(float(remaining_sec/4))))
+
+            return None
+
+
         elif c.protocolflag() == 'Location' and c.loctype() != 'Standard Location':
 
             co = coord2bin(latitude, longitude, 2, southnorth, eastwest)
@@ -116,25 +146,35 @@ def decode(hex_code):
 
 
         nh = bin2hex(binstr[1:])
-        c.processHex(nh)
-        newline = '\n{l} {beacon}\n{sn} {deglat}-{ew} {deglong}. \nHex: {longh} {m}.\n15 Hex:{h15}:{test}\n'.format(
-            m=c.type, l=c.loctype(), sn=tsouthnorth, ew=teastwest, deglat=latitude / 1000,
-            deglong=longitude / 1000, longh=nh, h15=c.hex15, oldh=hex_code, test=c.hex15 == hex_code.upper(),
-            beacon=c.btype())
 
-
-
-        print(newline)
-        print(c.protocoldata() + '\n')
-        print(c.countrydetail.countrydata() + '\n')
-
-
-
-
-
-        print('{bch1}\n{bch2}\n'.format(mtype=c.type, bch1=c.bch.writebch1(), bch2=c.bch.writebch2()))
+        return nh
 
     except decodehex2.HexError as e:
         print(e.value, e.message)
 
-print(decode('A78DC882F0411ED'))
+
+if __name__ == "__main__":
+    serialuser = 'A78DC882F0411ED'
+    eltdt ='2792E26E3DBFDFF'
+    hex_code = eltdt
+    latitude = randint(0, 89999)
+    longitude = randint(0, 179999)
+    southnorth = randint(0, 1)
+    eastwest = randint(0, 1)
+    tsouthnorth = ('North', 'South')[southnorth]
+    teastwest = ('East', 'West')[eastwest]
+
+
+    longhex=decode(hex_code,latitude,southnorth,longitude,eastwest)
+
+    c = decodehex2.BeaconHex(longhex)
+
+    newline = '\n{l} {beacon}\n{sn} {deglat}-{ew} {deglong}. \nHex: {longh} {m}.\n15 Hex:{h15}:{test}\n'.format(
+        m=c.type, l=c.loctype(), sn=tsouthnorth, ew=teastwest, deglat=latitude / 1000,
+        deglong=longitude / 1000, longh=longhex, h15=c.hex15, oldh=hex_code, test=c.hex15 == hex_code.upper(),
+        beacon=c.btype())
+
+    print(newline)
+    print(c.protocoldata() + '\n')
+    print(c.countrydetail.countrydata() + '\n')
+    print('{bch1}\n{bch2}\n'.format(mtype=c.type, bch1=c.bch.writebch1(), bch2=c.bch.writebch2()))
