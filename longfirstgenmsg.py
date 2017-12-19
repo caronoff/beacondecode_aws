00060ba1aafe1a51cc13dc61efe906fce84336b7
caronoff <caronoff@gmail.com>
2017-12-18 22:25:41 -0500

    longfirstgen National Location calculation

--- longfirstgenmsg.py
+++ longfirstgenmsg.py
@@ -119,7 +119,7 @@ if __name__ == "__main__":
     rls='279A8180103FDFF'
     nat ='A79000000000000'
     nat_loc='27942D56BF81FE0'
-    hex_code = serialuser
+    hex_code = nat_loc
     latitude = randint(0, 89999)
     longitude = randint(0, 179999)
     southnorth = randint(0, 1)
