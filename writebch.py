59b3fdb09cc3a5cdf1f27b11d623d0ab4a6a7743
CAronoff <caronoff@gmail.com>
2018-02-27 15:52:41 -0500

    write bch file

--- writebch.py
+++ writebch.py
@@ -60,7 +60,7 @@ def calcBCH(binary, b1start, b1end, b2end):
     f.write(bchfinalw)
     sout=sout+bchfinalw
     f.write("\nBCH code (last 48 bits.)\n{}\n{}\n{}".format(48*'-',bchfinal,48*'-'))
-    sout=sout+'\nBCH code (last 48 bits.)\n{}\n"{}\n{}'.format(48*'-',bchfinal,48*'-')
+    sout=sout+'\nBCH code (last 48 bits.)\n{}\n{}\n{}'.format(48*'-',"'"+bchfinal,48*'-')
     f.close()
 
     return (bchfinal,sout)
