595e022f6d7dd78209aeef3e2e9770057090ffd3
CAronoff <caronoff@gmail.com>
2018-09-06 16:11:03 -0400

    country2.csv

--- __init__.py
+++ __init__.py
@@ -135,6 +135,8 @@ def validatehex():
     hexaPattern = re.findall(r'([A-F0-9])', ret_data,re.M|re.I)
     statuscheck='not valid'
     message = 'Enter a valid beacon hex message'
+    print(vlengths)
+    print(type(vlengths))
     if len(ret_data) > 0:
         if len(hexaPattern)==len(ret_data):
             message='Valid hexidecimal message.'
