14c466c8a183a836568ea1e4552c065405b66c3b
CAronoff <caronoff@gmail.com>
2018-10-01 11:32:34 -0400

    xml feature

--- typeapproval.py
+++ typeapproval.py
@@ -1,17 +1,31 @@
 import xml.etree.ElementTree as ET
 tree = ET.parse('joomla3_cospas_sarsat_tac_beacons.xml')
 root = tree.getroot()
+
 def tac(num,flds):
     for child in root:
         d={}
+        i=[]
+        c=0
         for row in child:
             for t in row.iter('column'):
                 if t.get('name') == 'tac_number':
+
                     if t.text==str(num):
+                        print(t.text)
+                        c+=1
+                        d={}
                         for x in row:
                             for f in flds:
                                 if f==x.get('name'):
                                     d[f]=x.text
-    return d
+                        i.append(d)
+    print(c)
+    return i[0]
 if __name__ == "__main__" :
-    print(tac(30654,['battery','tested_life','ddd']))
+    models=tac(194,['id','name','battery','model_add_names','tested_life','ddd'])
+
+    for d in models:
+        for k in d:
+            print(k,d[k])
+    print(models)
