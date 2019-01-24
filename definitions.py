76d9e2c8e4c6b325e81d8b6881cf105c62246a23
CAronoff <caronoff@gmail.com>
2019-01-24 10:40:14 -0500

    T018 Issue 1 Rev 4

--- definitions.py
+++ definitions.py
@@ -7,6 +7,7 @@
 # Includes 1st and 2nd Gen 
 
 import re
+from moreinfo import moreinfo
 
 def is_number(s):
     try:
@@ -814,38 +815,4 @@ baudot2 = {'': '100000', ' ': '100100', '-': '011000', '/': '010111', '1': '0111
            'K': '111110', 'J': '111010', 'M': '100111', 'L': '101001', 'O': '100011', 'N': '100110',
            'Q': '111101', 'P': '101101', 'S': '110100', 'R': '101010', 'U': '111100', 'T': '100001',
            'W': '111001', 'V': '101111', 'Y': '110101', 'X': '110111', 'Z': '110001'}
-
-
-moreinfo= {'sgb_radio_callsign':('sgb_radio_callsign',
-                                 '''The Radio call sign binary data is decoded using the modified-Baudot code shown in Table 3.1a of document T.018. This enables 7 characters to be encoded with 6 bits per character using only 42 bits (6x7=42).
-                                     The two highest bits are spare and shall be coded as 00. This data will be left justified
-                                    with a modified-Baudot space (100100) being used where no character exists. 
-                                    If no Radio call sign is available then insert a series of 7 spaces (100100) '''),
-
-           'country_code':('country_code','''
-           The country code is a three-digit decimal Maritime Identification Digit (MID) country code assigned by the International Telecommunication Union (ITU). 
-           The MID country code and corresponding country names are available on the ITU website (www.itu.int).           
-           '''),
-
-           'rls_trunc':('rls_trunc',
-            '''
-           Truncated RLS TAC or National RLS Number
-The 10-bit RLS truncated TAC or National RLS number is the last 3 decimal numbers in the TAC number data field, which allows a range of 1 to 999. The RLS beacon TAC number or National RLS number series are assigned as follows:	
-1000 series is reserved for EPIRBs (i.e. 1001 to 1999),
-2000 series is reserved for ELTs (i.e. 2001 to 2999), and
-3000 series is reserved for PLBs (i.e. 3001 to 3999).
-
-These are represented in the RLS messages as bits 16-17 indicating the beacon type series (EPIRB, ELT, or PLB) and a 10 bit (1-1024) TAC number which is added to the series to encode the full TAC number. (e.g., TAC 1042 would be encoded as “01” for EPIRB in bits 41 to 42 representing 1nnn, where “nnn” is “042” determined by “0000101010” in bits 18 to 27, and form a binary representation “00 0010 1010” of the decimal number “42”.  
-
-The last 30 numbers (i.e., National RLS Numbers 970 to 999) are set aside for National Use by Competent Authorities. That is full National RLS Numbers 1970 to 1999 for EPIRBs, 2970 to 2999 for ELTs and 3970 to 3999 for PLBs.
-          
-           
-           '''),
-
-        'elt_dt_aircraftoperator':('elt_dt_aircraftoperator','''
-       Aircraft operator designator (3 letters) can be encoded in 15 bits using a shortened form of the
-       modified-Baudot code (i.e.: all letters in the modified-Baudot code are coded in 6 bits, with the first bit = "1".
-       This first bit can, therefore, be deleted to form a 5-bit. 3 x 5 = 15 bits''')
-
-                        }
 
