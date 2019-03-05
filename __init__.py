1743a20141ab6a8e4d02cd7ccc9bd938ab81f4c5
CAronoff <caronoff@gmail.com>
2019-03-05 10:56:56 -0500

    Decode short message

--- __init__.py
+++ __init__.py
@@ -302,7 +302,7 @@ def decoded(hexcode):
                                tacflds=tflds,
                                showmenu=MENU)
     except decodehex2.HexError as e:
-        print(e)
+        print(e[1])
 
 @app.route("/bch/<hexcode>")
 def download_bch(hexcode):
