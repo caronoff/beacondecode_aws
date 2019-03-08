b2dd273635fb1b7e8e102e4006d389443bed5aee
CAronoff <caronoff@gmail.com>
2019-03-08 12:16:38 -0500

    Decode short message

--- Gen2secondgen.py
+++ Gen2secondgen.py
@@ -309,7 +309,7 @@ class SecondGen(Gen2Error):
                 self.calculatedBCH =Func.calcBCH(self.bits[1:], 0, 202, 250)
                 hexBCH= Func.bin2hex(self.calculatedBCH)
                 self.tablebin.append(['203-250', self.calculatedBCH, 'Calculated BCH', 'Hex Value: {}'.format(hexBCH)])
-                self.tablebin.append(['', '', 'Hex Value: {}{}'.format(self.inputhex,hexBCH),''])
+                self.tablebin.append(['Complete Message', '', 'Hex Value: {}{}'.format(self.inputhex,hexBCH),''])
 
 
 
