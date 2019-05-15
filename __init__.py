4b37fee64782a15f7b82f695f67dd6ec9e6227af
CAronoff <caronoff@gmail.com>
2019-05-15 16:21:21 -0400

    filter valid hex

--- __init__.py
+++ __init__.py
@@ -24,10 +24,26 @@ COUNTRIES=[]
 country=open('countries2.csv')
 for line in country.readlines():
     COUNTRIES.append(line)
-##for key in definitions.countrydic:
-##    COUNTRIES.append('{} ({})'.format(definitions.countrydic[key], key))
 COUNTRIES.sort()
 
+class Book(db.Model):
+    title = db.Column(db.String(80), unique=True, nullable=False, primary_key=True)
+    def __repr__(self):
+        return "<Title: {}>".format(self.title)
+
+@app.route("/add", methods=["GET", "POST"])
+def home():
+    books = None
+    if request.form:
+        try:
+            book = Book(title=request.form.get("title"))
+            db.session.add(book)
+            db.session.commit()
+        except Exception as e:
+            print("Failed to add book")
+            print(e)
+    books = Book.query.all()
+    return render_template("home.html", books=books)
 
 
 @app.route('/validatehex', methods=['GET'])
