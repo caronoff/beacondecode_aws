a785a02b2d596abd48135d90cc904a2c3c8f5e8f
CAronoff <caronoff@gmail.com>
2019-05-22 15:01:47 -0400

    filter valid hex

--- __init__.py
+++ __init__.py
@@ -77,7 +77,7 @@ def login():
 
         user = Userlogin.query.filter_by(uname=form.username.data).first()
         if user is not None:
-            #login_user(form.user)
+            login_user(user)
             flash('Logged in successfully.')
             return redirect(url_for('index'))
         else:
