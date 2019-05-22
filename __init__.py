da66f77384b5a759faa6ef882d31fd1fcae4bda3
CAronoff <caronoff@gmail.com>
2019-05-22 15:58:58 -0400

    filter valid hex

--- __init__.py
+++ __init__.py
@@ -41,21 +41,10 @@ class LoginForm(Form):
         Form.__init__(self, *args, **kwargs)
         self.user = None
 
+@login_manager.user_loader
+def load_user(user_id):
+    return Userlogin.query.filter_by(u_id =int(user_id)).first()
 
-
-@login_manager.request_loader
-def load_user(request):
-    token = request.headers.get('Authorization')
-    if token is None:
-        token = request.args.get('token')
-    if token is not None:
-        username,password = token.split(":") # naive token
-        user_entry = User.get(username)
-        if (user_entry is not None):
-            user = User(user_entry[0],user_entry[1])
-            if (user.password == password):
-                return user
-    return None
 
 
 @app.route("/protected/",methods=["GET"])
