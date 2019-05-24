7fa8aa7471b3f6728f4bdfe904a4ab96ed198381
CAronoff <caronoff@gmail.com>
2019-05-24 09:58:18 -0400

    filter valid hex

--- __init__.py
+++ __init__.py
@@ -148,7 +148,7 @@ def login():
                 next_page = url_for('index')
             elif next_page and url_parse(next_page).netloc == '':
                 next_page = next_page
-            return redirect(url_for( next_page))
+            return redirect(url_for(next_page))
         else:
             flash('ERROR! Invalid login credentials')
 
