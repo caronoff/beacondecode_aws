2f2d6abf1fb933def7cda0edc07aa31e990fe300
CAronoff <caronoff@gmail.com>
2019-05-24 11:03:45 -0400

    filter valid hex

--- __init__.py
+++ __init__.py
@@ -140,20 +140,21 @@ def login():
     if request.method== 'POST' and form.validate():
         # Login and validate the user.
         # user should be an instance of your `Userlogin` class
-
-        user = Userlogin.query.filter_by(uname=form.username.data).first()
-        if user is None or not user.check_password(form.password.data):
-            flash('ERROR! Invalid login credentials')
-            return redirect(url_for('login'))
-            login_user(user)
-        session['logged_in']=True
-        flash('Logged in successfully.')
-        login_user(user, remember=True)
         next_page = request.args.get('next')
         if next_page:
             next_page = request.args.get('next').strip('/')
         if not next_page or url_parse(next_page).netloc != '':
-            next_page = url_for('index')
+            next_page = url_for('login')
+        else:
+            next_page = url_for('login')
+        user = Userlogin.query.filter_by(uname=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('ERROR! Invalid login credentials')
+            return redirect(url_for(next_page))
+
+        session['logged_in']=True
+        flash('Logged in successfully.')
+        login_user(user, remember=True)
 
         return redirect(url_for(next_page))
 
