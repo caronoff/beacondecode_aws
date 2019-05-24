6df8b19b55747df9f593636e80d6a4131f514653
CAronoff <caronoff@gmail.com>
2019-05-24 09:47:15 -0400

    filter valid hex

--- __init__.py
+++ __init__.py
@@ -143,9 +143,11 @@ def login():
             login_user(user)
             session['logged_in']=True
             flash('Logged in successfully.')
-            next_page = request.args.get('next').strip('/')
+            next_page = request.args.get('next')
             if not next_page or url_parse(next_page).netloc != '':
                 next_page = url_for('index')
+            elif next_page and url_parse(next_page).netloc == '':
+                next_page = next_page.strip('/')
             return redirect(url_for( next_page))
         else:
             flash('ERROR! Invalid login credentials')
