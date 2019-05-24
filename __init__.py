bc2edbb7dab93f849a2a7b732decd5bc26cad4a5
CAronoff <caronoff@gmail.com>
2019-05-24 14:35:34 -0400

    filter valid hex

--- __init__.py
+++ __init__.py
@@ -192,8 +192,9 @@ def home():
             print("Failed to add user")
             print(e)
     users = Userlogin.query.all()
-    #uid= Userlogin.query(Userlogin.u_id).order_by(Userlogin.u_id)[-1:]
-    #next_uid = int(uid)+1
+    uid= Userlogin.query(Userlogin.u_id)
+    print(uid)
+    next_uid = int(uid)+1
     return render_template("users.html", users=users,next_uid=5)
 
 
