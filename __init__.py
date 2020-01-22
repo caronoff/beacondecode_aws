8efed66815d1ca65149f385e4fc129018f91da33
CAronoff <caronoff@gmail.com>
2020-01-22 09:29:42 -0500

    T001 Issue 4 rev.5 for RLS MMSI

--- __init__.py
+++ __init__.py
@@ -31,7 +31,7 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
 
-print(gmap_key)
+print('hi'+gmap_key+'hi')
 MENU = False
 COUNTRIES=[]
 country=open('countries2.csv')
