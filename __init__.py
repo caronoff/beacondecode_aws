37f166ba2b77408d89d289fc50b174f07258bcd3
caronoff <caronoff@gmail.com>
2021-11-02 13:02:11 -0400

    routine testing version

--- __init__.py
+++ __init__.py
@@ -691,8 +691,13 @@ def submit_form():
         })
 
         return redirect(url_for('signs3target',fname=file_name))
-    flash("You must upload a file and select at least one field to process")
-    return render_template('account.html',flashType="danger")
+
+    elif request.method == "POST" and not (request.form["filename"] or request.form.getlist("choices")):
+        flash("You must upload a file and select at least one field to process")
+        return render_template('account.html',flashType="danger")
+    else:
+        return render_template('account.html', flashType="danger")
+
 
 
 
