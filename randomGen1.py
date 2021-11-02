22afc9e98c33b27c90c3c3de11bc774d972258b0
caronoff <caronoff@gmail.com>
2021-11-02 12:30:31 -0400

    routine testing version

--- randomGen1.py
+++ randomGen1.py
@@ -1,7 +1,8 @@
 import decodefunctions as Fcn
+import Gen2functions as sgb
 import random
 import decodehex2
-decoded=open('samplehex.csv','w')
+
 
 
 def randombinary(l):
@@ -16,25 +17,78 @@ def randombinary(l):
         binstr = binstr + b
     return binstr
 
+def random_error(hexvalid,pdf1err,pdf2err,sgberr):
+    #pdf1/bc1 errors correctible 3
+    #pdf2/bc2 errors correctible 2
+    scramble=list( Fcn.hextobin(hexvalid))
+    #print(scramble)
+    b=Fcn.hextobin(hexvalid)
+    if pdf1err or pdf2err:
+        for i in range(pdf1err):
+            epos=random.randint(0,81)
+
+            scramble[epos] = str(int(not int(scramble[epos])))
+
+        for j in range(pdf2err):
+            epos = random.randint(82, 119)
+
+            scramble[epos] = str(int(not scramble[epos]))
+    elif sgberr:
+        for i in range(sgberr):
+            epos = random.randint(0, 251)
+            scramble[epos] = str(int(not int(scramble[epos])))
+
+
+    return  Fcn.bin2hex(''.join(scramble))
 
 preamble='000000000000000000101111'
-
 short_long='1'
 user_location='1'
 countrycode='0100111100'  #316 - Canada
 
+def fgb(f,r):
+    for n in range(r):
+        ident=randombinary(49)
+        while ident[:4]=='101' and user_location=='1':
 
-for n in range(10000):
+            ident=randombinary(49)
 
-    ident=randombinary(49)
-    pdf1=preamble+short_long+user_location+countrycode+ident
-    bch1= Fcn.calcbch(('_'+pdf1+'0'*59), "1001101101100111100011", 25, 86, 107)
-    newbin=pdf1+bch1
-    supdata=randombinary(26)
-    bch2 = Fcn.calcbch('_'+newbin+supdata+'0'*12, '1010100111001', 107, 133, 145)
-    finalbin=newbin+supdata+bch2
-    # print(Fcn.bin2hex(finalbin))
-    hexcheck=Fcn.bin2hex(finalbin)
-    if decodehex2.BeaconFGB(hexcheck).errors==[]:
-        decoded.write(hexcheck+'\n')
-decoded.close()
+        pdf1=preamble+short_long+user_location+countrycode+ident
+        bch1= Fcn.calcbch(('_'+pdf1+'0'*59), "1001101101100111100011", 25, 86, 107)
+        newbin=pdf1+bch1
+
+        supdata=randombinary(26)
+
+        bch2=''
+        try:
+            bch2 = Fcn.calcbch('_'+newbin+supdata+'0'*12, '1010100111001', 107, 133, 145)
+        except IndexError:
+            print('error')
+            print(len('_'+newbin+supdata+'0'*12))
+        finalbin=newbin+supdata+bch2
+
+        hexcheck=Fcn.bin2hex(finalbin[24:])
+
+        f.write(random_error(hexcheck,random.randint(0,4),random.randint(0,3),0)+'\n')
+
+
+
+def sgbcompute(f,r):
+    for n in range(r):
+        pdf = randombinary(202).zfill(202)
+
+        bchcalc= sgb.calcBCH( pdf + '0'*48, 0, 202, 250)
+
+        finalbin = (pdf+bchcalc).zfill(252)
+
+        hex = Fcn.bin2hex(finalbin)
+
+        f.write(random_error(hex, 0,0, random.randint(0, 8)) + '\n')
+
+
+
+
+if __name__ == "__main__":
+    f = open('samplehex.csv', 'a')
+    sgbcompute(f,500)
+    f.close()
